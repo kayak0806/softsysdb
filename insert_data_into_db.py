805af8c47ce2d848f5b288540ec5149174edde84
Eric Schneider <franz.eric.schneider@gmail.com>
2014-03-02 18:01:54 -0500

    Added python database accessers

--- insert_data_into_db.py
+++ insert_data_into_db.py
@@ -3,18 +3,20 @@ import csv
  
 connection = pynuodb.connect("test", "localhost", "dba", "goalie", options={'schema':'hockey'})
 cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
-sql = """CREATE TABLE EMPLOYEE (
-         ID int not null primary key,
-         NAME 			VARCHAR(60),
+cursor.execute("DROP TABLE IF EXISTS ACTOR")
+sql = """CREATE TABLE ACTOR (
+         ID int NOT NULL AUTO_INCREMENT primary key,
+         NAME 		VARCHAR(60),
          MOVIE   	VARCHAR(150))"""
 cursor.execute(sql)
 
-with open('actors.csv')
-while 
-employee_val=(123098, "NuoDB", "Admin", "215 First St", "Cambridge", "MA", 02142, 6175000001)
-cursor.execute("INSERT INTO EMPLOYEE VALUES (?,?,?,?,?,?,?,?)", employee_val)
-cursor.execute("SELECT * FROM EMPLOYEE")
+with open('actors.csv', 'rb') as csvfile:
+	actorReader = csv.reader(csvfile, delimiter=',')
+	for row in actorReader:
+		actor_val=(row[0]+" "+row[1], row[2])
+		cursor.execute("INSERT INTO ACTOR VALUES (?,?)", actor_val)
+
+cursor.execute("SELECT * FROM ACTOR")
 print cursor.fetchone()
 
 connection.close()
